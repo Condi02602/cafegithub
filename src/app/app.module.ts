66d3d9fe71813de4c538455df67f19c223beff15
Conrad <conradaruldas2602@gmail.com>
2024-02-05 13:49:52 +0530

    initial commit imports done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,94 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatError, MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import { LoginComponent } from './login/login.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
+import { SignupComponent } from './signup/signup.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { CustomSidebarComponent } from './sidebar/custom-sidebar/custom-sidebar.component'
+import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router';
+import { EmployeeComponent } from './employee/employee.component';
+import { EmpAddEditComponent } from './emp-add-edit/emp-add-edit.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import { HttpClientModule } from '@angular/common/http';
+import { DatePipe } from '@angular/common';
+import {MatTableModule} from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { MenuItemsComponent } from './menu-items/menu-items.component';
+import { CartComponent } from './cart/cart.component';
+import { PaymentGatewayComponent } from './payment-gateway/payment-gateway.component';
+import { GooglePayButtonModule } from '@google-pay/button-angular';
+import { BillsComponent } from './bills/bills.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HomeComponent,
+    LoginComponent,
+    SignupComponent,
+    SidebarComponent,
+    CustomSidebarComponent,
+    EmployeeComponent,
+    EmpAddEditComponent,
+    MenuItemsComponent,
+    CartComponent,
+    PaymentGatewayComponent,
+    BillsComponent,
+    UserComponent,
+
+    
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    RouterModule,
+    MatDialogModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatRadioModule,
+    MatSelectModule,
+    HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatSnackBarModule,
+    GooglePayButtonModule
+    ],
+  providers: [DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
